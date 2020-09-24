e6ae87072698195b599db4474063446a8a1485a5
Rune Rønneseth <rune.ronneseth@kred.no>
2020-09-24 02:44:56 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ terraform {
     null = {
       # { this line is included due to visual bug in editor, and serves no purpose }
       source = "hashicorp/null"
-      version = "<= 2.1.2"
+      version = ">= 2.1.2"
     }
   }
 }
